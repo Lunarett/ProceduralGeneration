b889ead8310eaa21730fb49dd23635a1f5c157ea
Egor <e.ageev@saedubai.com>
2020-11-19 00:54:12 +0100

    Made first building, started ai (doesn't work)

---
+++ Assets/Scripts/AI/Enemy.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+	public enum States
+	{
+		Idle,
+		Chase
+	};
+
+	[SerializeField] private States _enemyState;
+	[SerializeField] private GameObject _target;
+
+	private bool _inRange;
+	private NavMeshAgent _agent;
+
+
+
+	private void Awake()
+	{
+		_agent = GetComponent<NavMeshAgent>();
+	}
+
+	private void Update()
+	{
+		SetSate();
+
+		switch (_enemyState)
+		{
+			case States.Idle:
+				OnIdle();
+				break;
+			case States.Chase:
+				OnChase();
+				break;
+			default:
+				break;
+		}
+	}
+
+	private void SetSate()
+	{
+		if(_inRange)
+		{
+			_enemyState = States.Chase;
+		}
+		else
+		{
+			_enemyState = States.Idle;
+		}
+	}
+
+	private void OnIdle()
+	{
+		Debug.Log(_enemyState);
+		_agent.SetDestination(transform.position);
+
+		/*
+		 * Ai should go from waypoint 1 to waypoint 2 in the building
+		 * needs sight detection
+		 */
+	}
+
+	private void OnChase()
+	{
+		Debug.Log(_enemyState);
+		_agent.SetDestination(_target.transform.position);
+
+		transform.LookAt(_target.transform);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+			_inRange = true;
+	}
+	private void OnTriggerExit(Collider other)
+	{
+		_inRange = false;
+	}
+}
--- Assets/Vegetation.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Vegetation : MonoBehaviour
-{
-	[SerializeField] private int _min = 1;
-	[SerializeField] private int _max = 10;
-}
